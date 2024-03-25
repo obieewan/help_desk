f86a00a7d64af654446cc2d35a05bccdedcc30c0
Obie <obie.tizon@gmail.com>
2024-03-25 13:13:44 +0800

    Set subject by default not nil and add status by default open and not nil

--- lib/helpdesk/support/ticket.ex
+++ lib/helpdesk/support/ticket.ex
@@ -13,6 +13,25 @@ defmodule Helpdesk.Support.Ticket do
     uuid_primary_key :id
 
     # Add a string type attribute called `:subject`
-    attribute :subject, :string
+    attribute :subject, :string do
+ # Don't allow `nil` values
+      allow_ni? false
+      end
+  end
+
+ # status is either `open` or `closed`. We can add more statuses later
+  attribute :status, :atom do
+    # Constraints allow you to provide extra rules for the value.
+    # The available constraints depend on the type
+    # See the documentation for each type to know what constraints are available
+    # Since atoms are generally only used when we know all of the values
+    # it provides a `one_of` constraint, that only allows those values
+    constraints [one_of: [:open, :closed]]
+
+    # The status defaulting to open makes sense
+    default :open
+
+    # We also don't want status to ever be `nil`
+    allow_nil? false
   end
 end
